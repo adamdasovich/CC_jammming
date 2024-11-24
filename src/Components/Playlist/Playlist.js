c5dc20fa04d1b6ba248f4363d1e68c5cd963d56b
Adam Dasovich <adamdasovich@gmail.com>
2024-11-24 08:13:14 -0500

    first

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Playlist.module.css'
+import Tracklist from '../Tracklist/Tracklist'
+
+const Playlist = () => {
+  return (
+    <div>Playlist</div>
+  )
+}
+
+export default Playlist
